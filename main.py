90b9082815a8c1cc72eb193de07aab957ebdb0aa
EKProgrammer <egor.s.korchagin@gmail.com>
2021-03-19 20:28:30 +0400

    solution

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, redirect
+
+from data.jobs import Jobs
+from data.users import User
+from data import db_session
+from forms.registerform import RegisterForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.repeat_password.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data,
+            hashed_password=form.password.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address = form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Главная страница')
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
